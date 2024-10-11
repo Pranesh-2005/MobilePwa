4ef6e2ce26486afd6f9d5776e25201cebaf61d45
Pranesh-2005 <150977208+Pranesh-2005@users.noreply.github.com>
2024-10-11 12:45:10 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,11 +1,7 @@
 document.getElementById('analyzeButton').addEventListener('click', async () => {
     const inputText = document.getElementById('inputText').value;
 
-<<<<<<< HEAD
-    const response = await fetch('https://mobilepwa.onrender.com/analyze', { // Replace with actual URL when deployed
-=======
     const response = await fetch('/analyze', { // Use relative URL for the API endpoint
->>>>>>> 37fc3e5 (Initial commit)
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
