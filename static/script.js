202102cccc8a1a8e55c214d020c71b1c8455b365
Pranesh-2005 <150977208+Pranesh-2005@users.noreply.github.com>
2024-10-11 11:34:10 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 document.getElementById('analyzeButton').addEventListener('click', async () => {
     const inputText = document.getElementById('inputText').value;
 
-    const response = await fetch('http://localhost:7070/analyze', { // Replace with actual URL when deployed
+    const response = await fetch('https://mobilepwa.onrender.com/analyze', { // Replace with actual URL when deployed
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
